43ef33269667f502c057a86eef83babd5ab27cb2
himanshu-tiwari <htiwarih@gmail.com>
2019-02-24 23:56:33 +0530

    Added details modal

---
+++ src/Components/Listing/ContactDetailsModal/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './index.scss';
+import { ReactComponent as Close } from '../../../assets/icons/close.svg';
+import { ReactComponent as Phone } from '../../../assets/icons/phone-call.svg';
+import { ReactComponent as Email } from '../../../assets/icons/email.svg';
+import { ReactComponent as Suitcase } from '../../../assets/icons/suitcase.svg';
+import { ReactComponent as Cake } from '../../../assets/icons/cake.svg';
+import moment from 'moment';
+
+const ContactDetailsModal = (props) => {
+    const { name, avatarColor, toggleModal, phonelist, emaillist, company, title, birthday } = props;
+
+    return(
+        <div className="contact-details-modal-container">
+            <div className="contact-details-modal">
+                <Close className="close" onClick={toggleModal} />
+                <h3 className="modal-heading">
+                    <span
+                        className="avatar"
+                        style={{
+                            background: "#"+avatarColor
+                        }}
+                    >
+                        { name.charAt(0) }
+                    </span>
+                    { name }
+                </h3>
+                
+                <div className="modal-content">
+                    <h5 className="heading">Contact Details</h5>
+
+                    <div className="phone">
+                        <Phone />
+
+                        {
+                            phonelist.map((phone, i) => <p key={i}>
+                                { phone.num }
+                                <span className="type"> . { phone.type }</span>
+                            </p>)
+                        }
+                    </div>
+
+                    <div className="email">
+                        <Email />
+
+                        {
+                            emaillist.map((email, i) => <p key={i}>
+                                { email.email }
+                                <span className="type"> . { email.type }</span>
+                            </p>)
+                        }
+                    </div>
+
+                    {
+                        (company || title)
+                        ? <div className="work">
+                            <Suitcase />
+                            { title ? <p>{title}</p> : '' }
+                            { title && company ? <p className="at">at</p> : '' }
+                            { company ? <p>{company}</p> : '' }
+                        </div>
+                        : ''
+                    }
+
+                    {
+                        birthday
+                        ? <div className="birthday">
+                            <Cake />
+                            <p>{ moment(new Date(birthday)).format("MMM DD, YYYY") }</p>
+                        </div>
+                        : ''
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactDetailsModal;
--- src/Components/Listing/ContactRow/index.js
+++ src/Components/Listing/ContactRow/index.js
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.scss';
+import ContactDetailsModal from '../ContactDetailsModal';
 
-const ContactRow = (props) => {
-    console.log(Math.floor(Math.random()*16777215).toString(16));
-    return(
-        <div className="contact-row">
-            <p className="name">
-                <span
-                    className="avatar"
-                    style={{
-                        background: "#"+Math.floor(Math.random()*16777215).toString(16)
-                    }}
-                >
-                    { props.name.charAt(0) }
-                </span>
-                {props.name}
-            </p>
-            <p className="email">{props.emaillist[0].email}</p>
-            <p className="phone">{props.phonelist[0].num}</p>
-            <p className="location">{props.location}</p>
-            
-        </div>
-    );
+class ContactRow extends Component {
+    state = {
+        detailsModalVisible: false,
+        avatarColor: Math.floor(Math.random()*16777215).toString(16)
+    };
+
+    toggleModal = () => {
+        this.setState({
+            ...this.state,
+            detailsModalVisible: !this.state.detailsModalVisible
+        });
+    };
+
+    render() {
+        const { name, emaillist, phonelist, location } = this.props;
+
+        return(
+            <div className="contact-details-container">
+                <div className="contact-row" onClick={this.toggleModal}>
+                    <p className="name">
+                        <span
+                            className="avatar"
+                            style={{
+                                background: `#${this.state.avatarColor}`
+                            }}
+                        >
+                            { name.charAt(0) }
+                        </span>
+                        { name }
+                    </p>
+                    <p className="email">{ emaillist[0].email }</p>
+                    <p className="phone">{ phonelist[0].num }</p>
+                    <p className="location">{ location }</p>
+                </div>
+
+                {
+                    this.state.detailsModalVisible &&
+                    <ContactDetailsModal
+                        {...this.props}
+                        avatarColor={this.state.avatarColor}
+                        toggleModal={this.toggleModal}
+                    />
+                }
+            </div>
+        );
+    }
 };
 
 export default ContactRow;
